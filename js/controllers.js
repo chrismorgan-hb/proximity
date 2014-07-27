e688c2e5d140243ac7f202995b2f7fc24bd10898
Chris Morgan <christopher.j.morgan@gmail.com>
2014-07-27 16:57:24 -0700

    Add the ability for users to move the marker around

--- js/controllers.js
+++ js/controllers.js
@@ -6,12 +6,25 @@ angular.module('proximity.controllers', []).
       ga('send', 'pageview', {title: 'Home'});
       $scope.userLocationMarker = {
         coords: {
-          latitude: 45,
-          longitude: -73
+          latitude: 33.744433,
+          longitude: -118.015762
         }
       };
       $scope.map = {
           center: $scope.userLocationMarker.coords,
-          zoom: 8
+          zoom: 13,
+          events: {
+            click: function (mapModel, eventName, originalEventArgs) {
+              var e = originalEventArgs[0];
+              var lat = e.latLng.lat(), lon = e.latLng.lng();
+              $scope.userLocationMarker = {
+                coords: {
+                  latitude: lat,
+                  longitude: lon
+                }
+              };
+              $scope.$apply();
+            }
+          }
       };
   }]);
